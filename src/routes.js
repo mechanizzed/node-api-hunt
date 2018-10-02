c903b3795ca7ed9c60b9e4787d61c2bded877ca4
Luciano Guerra <dsibr@hotmail.com>
2018-10-02 11:42:54 -0300

    Added show product

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 const ProductController = require('./controllers/Product/ProductController');
 
 routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
 routes.post('/products/store', ProductController.store);
 
 module.exports = routes;
