f849799c61a4ac04a870f49aa0fe0569f2f40265
Luciano Guerra <dsibr@hotmail.com>
2018-10-02 11:51:52 -0300

    Added update controller

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const ProductController = require('./controllers/Product/ProductController');
 routes.get('/products', ProductController.index);
 routes.get('/products/:id', ProductController.show);
 routes.post('/products/store', ProductController.store);
+routes.put('/products/update/:id', ProductController.update);
 
 module.exports = routes;
