f074f216c9f6aeaf0f45c53df477deea6dafd9fb
Luciano Guerra <dsibr@hotmail.com>
2018-10-01 21:20:49 -0300

    Setup mongoose connection in server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//mongose - database
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapihunt',
+  { useNewUrlParser: true }
+);
 
 app.get('/', (req, res) => {
   res.send('Hello World teste');
