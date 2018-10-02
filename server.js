bf7a00cb7e695b12b2789183dde0a126ba5e055a
Luciano Guerra <dsibr@hotmail.com>
2018-10-01 21:37:17 -0300

    Model Product created

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
 
@@ -8,6 +9,8 @@ mongoose.connect(
   'mongodb://localhost:27017/nodeapihunt',
   { useNewUrlParser: true }
 );
+
+requireDir('./src/models');
 
 app.get('/', (req, res) => {
   res.send('Hello World teste');
